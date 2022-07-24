d64c68c7a6528aabba6f838fa3657cffd041cd13
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-24 21:23:18 +0500

    Увелечение лайков и дизлайков

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>
+        Vue.js
+    </h1>
+    <div class="buttons">
+        <button v-on:click="addLike">Увеличить лайки</button>
+        <button v-on:click="addDislike">Увеличить дизлайки</button>
+    </div>
+    <div>
+        Работает <span class="likes"><strong>Кол-во лайков {{ likes}} и дизлайков {{ dislikes}} </strong></span>
+    </div>    
+</template>
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 5,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes++;
+        },
+        addDislike() {
+            this.dislikes++;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
