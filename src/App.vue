bdbde4c344d8fc31f0d7755c629459c9d511dde7
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-25 10:16:52 +0500

    Добавление постов в data

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,89 @@
     <h1>
         Vue.js
     </h1>
-    <div class="buttons">
-        <button @click="addLike">Увеличить лайки</button>
-        <button @click="addDislike">Увеличить дизлайки</button>
+    <form class="posts-form" action="" @submit.prevent>
+        <h4 class="posts-form__title">Создание поста</h4>
+        <input 
+            v-bind:value="title"
+            @input="title = $event.target.value"
+            class="posts-form__input"
+            type="text"
+            name="title"
+            placeholder="Название"
+        >
+        <input v-bind:value="body" @input="body = $event.target.value" class="posts-form__input" type="text" name="body" placeholder="Имя">
+        <button @click="createPost" class="posts-form__button">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+        <div><strong>Название: </strong>{{post.title}}</div>
+        <div><strong>Описание: </strong>{{post.body}}</div>
     </div>
-    <div>
-        Работает <span class="likes"><strong>Кол-во лайков {{ likes}} и дизлайков {{ dislikes}} </strong></span>
-    </div>    
 </template>
 <script>
 export default {
     data() {
         return {
-            likes: 0,
-            dislikes: 5,
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Описание JS'},
+                {id: 2, title: 'HTML', body: 'Описание HTML'},
+                {id: 3, title: 'CSS', body: 'Описание CSS'},
+                {id: 4, title: 'DRR', body: 'Описание DRR'}
+            ],
+            title: '',
+            body: ''
+            
         }
+        
     },
     methods: {
-        addLike() {
-            this.likes++;
-        },
-        addDislike() {
-            this.dislikes++;
+        
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
         }
     }
 }
 </script>
 
 <style>
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+
+    .post{
+        border: 2px red solid;
+        padding: 25px;
+        margin-top:15px;
+    }
+
+    .posts-form{
+        display: flex;
+        flex-direction: column;
+    }
+
+    .posts-form__title{
+        font-size: 2em;
+        text-align: center;
+    }
+
+    .posts-form__input{
+        width: 100%;
+        margin: 15px auto;
+        height: 50px;
+        width: 50%;
+    }
+
+    .posts-form__button{
+        height: 50px;
+        font-size: 1.5em;
+        width: 50%;
+        margin: 15px auto;
+    }
 
 </style>
