7ac4a91b014f125e36efdf260c83914c011e4c67
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-29 10:31:59 +0500

    Получение постов

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,55 @@
     <h1>
         Vue.js
     </h1>
+    <my-button @click="fetchPosts">Получить посты</my-button>
     <my-button @click="showModel">Добавить пост</my-button>
     <my-dialog v-model:show=dialogVisible>
         <post-form @create="createPost"/>
     </my-dialog>
     <post-list @delete="removePost" :posts="posts" />
-    
+    <div v-if="isPostLoading">Идет загрузка</div>
 </template>
 <script>
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import axios from 'axios';
 export default { 
     components:{
         PostList, PostForm
     },
     data() {
         return {
-            posts: [
-                {id: 1, title: 'JavaScript', body: 'Описание JS'},
-                {id: 2, title: 'HTML', body: 'Описание HTML'},
-                {id: 3, title: 'CSS', body: 'Описание CSS'},
-                {id: 4, title: 'DRR', body: 'Описание DRR'}
-            ],
-            dialogVisible: false,   
+            posts: [],
+            dialogVisible: false,
+            isPostLoading: true,   
         }
-        
     },
-        methods: {
-            createPost(post){
-                this.posts.push(post);
-            },
-            removePost(post){
-                this.posts = this.posts.filter(p => p.id !== post.id)
-            },
-            showModel(){
-                this.dialogVisible = true;
+    methods: {
+        createPost(post){
+            this.posts.push(post);
+        },
+        removePost(post){
+            this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showModel(){
+            this.dialogVisible = true;
+        },
+        async fetchPosts() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                this.posts = response.data;
+                this.isPostLoading = false;
+            } catch(e) {
+                console.log('Ошибка')
+            } finally {
+                this.isPostLoading = false;
             }
-        }
+        },
+    },
+    mounted() {
+        this.fetchPosts();
+    }
+    
 }
 </script>
 
@@ -48,9 +60,5 @@ export default {
         padding:0;
         box-sizing: border-box;
     }
-
-    
-
-    
 
 </style>
